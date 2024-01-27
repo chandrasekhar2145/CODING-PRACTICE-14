4b290cd73d3b084f10c77323d3374e3f919b2679
Chandrasekhar Kuruva <148301206+chandrasekhar2145@users.noreply.github.com>
2024-01-27 19:57:50 +0530

    Update index.js

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -1,57 +1,60 @@
-// Write your code here
 import {Component} from 'react'
 
 import './index.css'
 
-const headsCoinURL = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
+const HEADS_IMG_URL = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
 
-const tailsCoinURL = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
+const TAILS_IMG_URL = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
 
 class CoinToss extends Component {
-  state = {tossResultImg: headsCoinURL, headsCoin: 0, tailsCoin: 0}
+  state = {
+    tossResultImage: HEADS_IMG_URL,
+    headsCount: 0,
+    tailsCount: 0,
+  }
 
-  onChangeCoinsToss = () => {
-    const {headsCoin, tailsCoin} = this.state
-    const tossCoin = Map.floor(Map.random() * 2)
-    let tossImg = ''
-    let updateHeadsCoin = headsCoin
-    let updateTailsCoin = tailsCoin
+  onTossCoin = () => {
+    const {headsCount, tailsCount} = this.state
+    const toss = Math.floor(Math.random() * 2)
+    let tossImage = ''
+    let latestHeadsCount = headsCount
+    let latestTailsCount = tailsCount
 
-    if (tossCoin === 0) {
-      tossImg = headsCoinURL
-      updateHeadsCoin += 1
+    if (toss === 0) {
+      tossImage = HEADS_IMG_URL
+      latestHeadsCount += 1
     } else {
-      tossImg = tailsCoinURL
-      updateTailsCoin += 1
+      tossImage = TAILS_IMG_URL
+      latestTailsCount += 1
     }
     this.setState({
-      tossResultImg: tossImg,
-      headsCoin: updateHeadsCoin,
-      tailsCoin: updateTailsCoin,
+      tossResultImage: tossImage,
+      headsCount: latestHeadsCount,
+      tailsCount: latestTailsCount,
     })
   }
 
   render() {
-    const {tossResultImg, headsCoin, tailsCoin} = this.state
-    const totalCount = headsCoin + tailsCoin
+    const {tossResultImage, headsCount, tailsCount} = this.state
+    const totalCount = headsCount + tailsCount
+
     return (
       <div className="app-container">
-        <div className="coin-container">
+        <div className="coin-toss-container">
           <h1 className="heading">Coin Toss Game</h1>
-          <p className="para">Heads (or) Tails</p>
-          <img src={tossResultImg} alt="toss result" className="toss-result" />
-          <button
-            type="button"
-            className="coin-button"
-            onClick={this.onChangeCoinsToss}
-          >
+          <p className="description">Heads (or) Tails</p>
+          <img
+            src={tossResultImage}
+            alt="toss result"
+            className="toss-result-img"
+          />
+          <button type="button" className="button" onClick={this.onTossCoin}>
             Toss Coin
           </button>
-
-          <div className="coin-container-2">
-            <p className="count">Total:{totalCount}</p>
-            <p className="count">Heads:{headsCoin}</p>
-            <p className="count">Tails:{tailsCoin}</p>
+          <div className="counts-container">
+            <p className="count">Total: {totalCount}</p>
+            <p className="count">Heads: {headsCount}</p>
+            <p className="count">Tails: {tailsCount}</p>
           </div>
         </div>
       </div>
